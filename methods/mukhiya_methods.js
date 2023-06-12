5b81339d3eb1e1809ed362a8e556f3ebd19512f4
naitik gondaliya <devnaitik.g@gmail.com>
2023-06-12 21:46:55 +0530

    member fatch headline

--- methods/mukhiya_methods.js
+++ methods/mukhiya_methods.js
@@ -73,70 +73,19 @@ const mukhiyaLogin = async (req, res) => {
     }
 }
 
-const editHeadLine = async (req, res) => {
-    const headLine = req.body;
-    const headLineID = req.params.id;
-    const auth_token = req.headers['auth-token'];
-
-    if (headLine) {
-        const response = validation.editHeadLine(req.body)
-        try {
-            if (response.error) {
-                return res.status(200).send({ status: 0, msg: response.error.message });
-            }
-            else {
-                const data = response.value;
-
-                const adminDetail = await admin.findOne({
-                    where: {
-                        auth_token: auth_token,
-                    }
-                })
-                if (!adminDetail) {
-                    return res.status(203).json({ error: "wrong authenticator" });
-                }
-
-                const headLineDetail = await admin_headline.findOne({
-                    where: {
-                        admin_headline_id: headLineID,
-                    }
-                })
-
-                if (!headLineDetail) {
-                    return res.status(404).json({ error: "HeadLine not found" });
-                } else {
-                    await admin_headline.update({
-                        headline: data.headline
-                    }, {
-                        where: {
-                            admin_headline_id: headLineID,
-                        }
-                    })
-
-                    const headLineData = await admin_headline.findOne({
-                        where: {
-                            admin_headline_id: headLineID,
-                        }
-                    })
-
-                    res.status(200).send({ status: 1, msg: "edit head line successfull", data: headLineData });
-                }
-            }
-        } catch (error) {
-            res.status(500).send("Internal Server Error");
-        }
-    }
-}
-
 const fatchHeadLine = async (req, res) => {
     const auth_token = req.headers['auth-token'];
+
+    if (!auth_token) {
+        return res.status(404).send({ status: 0, msg: "auth token not found" });
+    }
     try {
-        const adminDetail = await admin.findOne({
+        const mukhiyaDetails = await mukhiya.findOne({
             where: {
                 auth_token: auth_token,
             }
         })
-        if (!adminDetail) {
+        if (!mukhiyaDetails) {
             return res.status(203).json({ error: "wrong authenticator" });
         } else {
             const headLineData = await admin_headline.findAll({})
@@ -150,87 +99,11 @@ const fatchHeadLine = async (req, res) => {
     }
 }
 
-const creatMember = async (req, res) => {
-    const memberDetails = req.body;
-    var auth_token = req.headers['auth-token'];
-
-    if (!auth_token) {
-        return res.status(404).send({ status: 0, msg: "auth token not found" });
-    }
-    const adminDetail = await admin.findOne({
-        where: {
-            auth_token: auth_token,
-        }
-    })
-    if (!adminDetail) {
-        return res.status(203).json({ error: "wrong authenticator" });
-    }
-
-
-    if (memberDetails && adminDetail) {
-
-        const response = validation.creatMember(req.body)
-
-
-        if (response.error) {
-            return res.status(200).send({ status: 0, msg: response.error.message });
-        }
-        else {
-
-            const data = response.value;
-
-            const mobile = await mukhiya.findOne({
-                where: {
-                    mukhiya_mobile_no: data.mukhiya_mobile_no,
-                }
-            })
-            if (mobile) {
-                return res.status(400).json({ error: 'User Mobile number already exist' })
-            }
-
-            const memberID = await mukhiya.findOne({
-                where: {
-                    member_id: data.member_id,
-                }
-            })
-            if (memberID) {
-                return res.status(400).json({ error: 'User Id already exist' })
-            }
-
-            const password = await bcrypt.hash(data.password, 10);
-
-            const member = await mukhiya.create({
-                member_id: data.member_id,
-                mukhiya_mobile_no: data.mukhiya_mobile_no,
-                member_password: password,
-                is_deleted: 0,
-                created_date: Date.now(),
-                updated_date: Date.now()
-
-            })
-
-            const memberdata = await mukhiya.findOne({
-                where: {
-                    mukhiya_mobile_no: data.mukhiya_mobile_no,
-                    member_id: data.member_id
-                }
-            })
-
-
-            res.status(200).send({ status: 1, msg: "create mukhiya member successfull", data: memberdata });
-
-        }
-
-    }
-}
-
 
 
 
 module.exports = {
     mukhiyaLogin,
-    editHeadLine,
     fatchHeadLine,
-    creatMember,
 
 }
