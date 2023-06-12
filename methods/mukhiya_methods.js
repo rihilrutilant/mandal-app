9af92303a28480c0d26d539dee258d4d8bb09a9c
naitik gondaliya <devnaitik.g@gmail.com>
2023-06-12 20:50:23 +0530

    chek

--- methods/mukhiya_methods.js
+++ methods/mukhiya_methods.js
@@ -59,12 +59,9 @@ const mukhiyaLogin = async (req, res) => {
                 const mukhiyaData = await mukhiya.findOne({
                     where: {
                         mukhiya_id: mukhiyaDetails.mukhiya_id,
-                    },
-                    attributes: ["auth_token"]
+                    }
                 })
-
                 console.log(mukhiyaData);
-                return;
 
                 res.status(200).send({ status: 1, msg: "login successfull", data: mukhiyaData });
 
